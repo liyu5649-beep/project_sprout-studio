fc1473a4602769fe58943b6d61a705be3fe58bb0
Ruby <liyu5649@gmail.com>
2025-12-26 16:11:48 +0800

    react 網頁檔案連結

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import Web from './Web.jsx'
+import { HashRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Web />
-  </StrictMode>,
+  <React.StrictMode>
+    <HashRouter>
+      <Web />
+    </HashRouter>
+  </React.StrictMode>,
 )
